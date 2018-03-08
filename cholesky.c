8bb56103141b2723a264c54402c4d951d8fa82c4
Jeremy Schmidt <jpschmid@ncsu.edu>
2018-03-07 22:23:35 -0500

    Write most of the cholesky decomposition functino (yet to compile), add test file

--- cholesky.c
+++ cholesky.c
@@ -12,13 +12,40 @@
 
 Matrix *choleskyDecomp(Matrix const *s)
 {
+    int n = s->rows;
     // Form a column of L based on valued from the first column of S
-
-    // The bottom part of that L is used with the remaining corner of S to form new S
+    Matrix *l = newMatrix(n, n);
+    double rp = sqrt(s->data[0][0]);
+    l->data[0][0] = rp;
+    for (int i = 1; i < n; ++i) {
+        // Copy V into lower part of L
+        l->data[i - 1][0] = s[i][0] / rp;
+    }
+    // Base case
+    if (n == 1) {
+        return l;
+    }
+    // The bottom part of that L is used with the remaining part of S to form new S
+    Matrix *v = newMatrix(n - 1, 1);
+    // Copy from bottom of L to v
+    for (int i = 1; i < n; ++i) {
+        v->data[i - 1][0] = l->data[i][0];
+    }
+    Matrix *s_prime = subtract(subMat(1, s->rows, 1, s->columns, s), multiply(v, transpose(v)));
 
     // Apply recursively on the now smaller S.
+    Matrix *l_prime = choleskyDecomp(s_prime);
     // New L's are padded with 0's to maintain NxN size
-
+    for (int i = 1; i < n; ++i) {
+        for (int k = 0; k < i; ++k) {
+            // Fill with zeros to make lower triangular
+            l->data[i][k] = 0;
+        }
+        // Copy from l_prime (result from recursive decomp)
+        for (int j = i; j < n; ++j) {
+            l->data[i][j] = l_prime[i - 1][j];
+        }
+    }
 }
 
 bool verify(Matrix *s, Matrix *l)
---
+++ choleskyTest.c
@@ -0,0 +1,15 @@
+/**
+ * @File choleskyTest.c
+ * Tests for Cholesky decomposition
+ * @author Jeremy Schmidt
+*/
+
+#include "cholesky.h"
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
+
